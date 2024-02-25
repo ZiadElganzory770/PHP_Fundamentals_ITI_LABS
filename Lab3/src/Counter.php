3ab18ce926c9e14162e4f296f678a47e850f3896
Ziad Mohamed Elganzory <ziadmohamed770@outlook.com>
2024-02-25 20:30:35 +0200

    Fix

--- Lab3/src/Counter.php
+++ Lab3/src/Counter.php
@@ -1,42 +1,5 @@
 <?php
 
-// namespace App;
-
-// class Counter {
-//     private $countFilePath;
-
-//     public function __construct($countFilePath) {
-//         $this->countFilePath = $countFilePath;
-//     }
-
-//     public function getCount() {
-//         if (file_exists($this->countFilePath)) {
-//             $content = file_get_contents($this->countFilePath);
-//             if (ctype_digit($content)) { 
-//                 return (int) $content;
-//             } else {
-//                 return -1;
-//             }
-//         } else {
-//             return 0;
-//         }
-//     }
-
-//     public function incrementCount() {
-//         $count = $this->getCount();
-//         if($count == -1){
-//             echo "Error: The content of the file is not a valid integer.";
-//         } elseif($count == 0){
-//             echo "File Not Exists";   
-//         } else {
-//             $count++;
-//             file_put_contents($this->countFilePath, $count);
-//         }
-
-//     }
-// }
-
-
 namespace App;
 
 
